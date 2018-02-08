282dc8bd206f881a29cba4341da9da2fde1e866b
Phil Turner <julietius@gmail.com>
2018-02-08 22:41:09 +0000

    add basic login handler with fake methods

--- src/index.js
+++ src/index.js
@@ -2,31 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { ButtonToolbar, Form, FormGroup, Col, ControlLabel, FormControl, Checkbox, Button } from 'react-bootstrap';
-import MonzoApi from './monzo-api.js';
 import MonzoAPI from './monzo-api.js';
 
-class Buttons extends React.Component{
+class Balance extends React.Component{
 	render(){
-		return(
-			<div className="center">
-			<ButtonToolbar>
-				{/* <button className = "btn btn-success" onClick={this.props.login}>
-						Login
-				</button>
-				<button className = "btn btn-warning" onClick={this.props.clear}>
-						Clear
-				</button> */}
-				</ButtonToolbar>
-			</div>
-		)
+			return(
+				<div>
+					<h2>Summary</h2>
+					<h4>Balance: £{this.props.balance}</h4>
+					<h4>Spent Today: £{this.props.spend_today}</h4>
+					<h4>Transactions: {this.props.transaction_count}</h4>
+					<h4>Total Value: £{this.props.transaction_total_value|0}</h4>
+				</div>
+			)
+		}
 	}
-}
+
+class LoginForm extends React.Component{
+	render(){
+			return(
+				<div className = "login-form">
+				<h2>Login</h2>
+				<Form horizontal>
+				<FormGroup controlId="formHorizontalEmail">
+					<Col sm={30}>
+						<FormControl type="text" placeholder="Acc ID" />
+					</Col>
+				</FormGroup>
+
+				<FormGroup controlId="formHorizontalPassword">
+					<Col sm={30}>
+						<FormControl type="password" placeholder="Token" />
+					</Col>
+				</FormGroup>
+
+				<FormGroup>
+					<Col smOffset={5} sm={30}>
+						<Button onClick = {this.props.loginHandler}>Login</Button>
+					</Col>
+				</FormGroup>
+			</Form>
+			</div>
+			)
+		}
+	}
 
 class Main extends React.Component{
 	constructor(){
 		super();
 			this.state = {
-				monzoData: {}
+				monzoData: {},
+				loggedIn: false
 			}
 		}
 
@@ -35,10 +61,15 @@ class Main extends React.Component{
 		let data = new MonzoAPI();
 		data.setBalance();
 		data.setSpendToday();
+		data.setTransCount();
+		data.setTotalTransValue();
 		this.setState({
+			loggedIn: true,
 			monzoData: data,
 			balance: data.balance,
-			spend_today: data.spend_today 
+			spend_today: data.spend_today,
+			transaction_count: data.transaction_count,
+			transaction_total_value: data.transaction_total_value
 		})
 	}
 
@@ -52,30 +83,30 @@ class Main extends React.Component{
 
 	render() {
 		return(
-	 	<div>
-			<h1>MONZO DASH</h1>
-			<Form horizontal>
-				<FormGroup controlId="formHorizontalEmail">
-					<Col sm={30}>
-						<FormControl type="text" placeholder="ID" />
-					</Col>
-				</FormGroup>
-
-				<FormGroup controlId="formHorizontalPassword">
-					<Col sm={30}>
-						<FormControl type="password" placeholder="Token" />
-					</Col>
-				</FormGroup>
-
-				<FormGroup>
-					<Col smOffset={5} sm={30}>
-						<Button onClick = {this.login}>Login</Button>
-					</Col>
-				</FormGroup>
-			</Form>
-			<h3>Balance: {this.state.balance}</h3>
-			<h3>Spent Today: {this.state.spend_today}</h3>
-		  </div>
+	 	<div className = "container">
+		 <div className= "left-nav">
+			{this.state.loggedIn
+			 ?
+				<Balance 
+					loggedIn = {this.state.loggedIn}
+					balance = {this.state.balance}
+					spend_today = {this.state.spend_today}
+					transaction_count = {this.state.transaction_count}
+					transaction_total_value = {this.state.transaction_total_value}
+				/> 
+				:
+				<LoginForm 
+					loginHandler = {this.login}
+				/>
+			}
+			</div>
+				<div className = "right-content">
+					<h2>Yar Pirate Ipsum</h2>
+					<p>Bring a spring upon her cable main sheet hempen halter me ballast lookout league code of conduct deadlights yo-ho-ho. Handsomely jib nipperkin take a caulk execution dock lanyard pirate scallywag Brethren of the Coast swab. Hands red ensign fire ship fathom Davy Jones' Locker Nelsons folly mizzen maroon parrel boom.</p>
+					<p>Pillage Nelsons folly Chain Shot tack line run a rig lugger coxswain bilge water holystone. Pieces of Eight run a shot across the bow Sail ho American Main snow booty ho quarter boom squiffy. Nipper Plate Fleet hardtack topsail Gold Road boatswain pinnace barque mizzenmast gaff.</p>				
+					<p>Sail ho run a shot across the bow poop deck keel fire ship Yellow Jack pressgang scallywag grog blossom bounty. Mizzen pressgang tackle cackle fruit booty Shiver me timbers lanyard Buccaneer scallywag rigging. Pinnace bring a spring upon her cable pink warp cable scallywag Letter of Marque Barbary Coast aft fire ship.</p>
+				</div>
+			</div>
 		);
 	}
 }
--- src/monzo-api.js
+++ src/monzo-api.js
@@ -1,8 +1,9 @@
-import testJson from './test.json'
+import balanceJSON from './balance.json'
+import transJSON from './trans.json'
 
 export default class MonzoAPI {
 	constructor(){
-		this.data = testJson;
+		this.data = balanceJSON;
 	}
 
 	setBalance = () => {
@@ -11,5 +12,23 @@ export default class MonzoAPI {
 
 	setSpendToday = () => {
 		this.spend_today = Math.abs(this.data.spend_today)/100;
+	}
+
+	setTransCount = () => {
+		let rawTransactions = transJSON;
+		let counter = 0;
+		Object.entries(rawTransactions.transactions).forEach(([key, value]) => {
+			if(value.include_in_spending) counter++;
+		});
+		this.transaction_count = counter;
+	}
+
+	setTotalTransValue = () => {
+		let rawTransactions = transJSON;
+		let total = 0;
+		Object.entries(rawTransactions.transactions).forEach(([key, value]) => {
+			if(value.include_in_spending) total += (Math.abs(value.amount)/100)
+		});
+		this.transaction_total_value = total;
 	}
 }
